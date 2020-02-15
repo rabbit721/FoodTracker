8a541cd6699b272607ea9e1fbbec192e1c6ca261
rabbit721 <001207jiaojie@gmail.com>
2020-02-14 21:38:43 -0500

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Katrina Jiao on 2020/2/14.
+//  Copyright © 2020 Katrina Jiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
